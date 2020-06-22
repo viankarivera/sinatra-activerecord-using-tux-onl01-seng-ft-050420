4e1956d83ae28b891814c8752aa6682ce76424e3
viankarivera <viankalrivera@gmail.com>
2020-06-22 18:44:36 -0400

    Done.

---
+++ db/migrate/20200622223328_create_cats.rb
@@ -0,0 +1,9 @@
+class CreateCats < ActiveRecord::Migration
+  def change
+    create_table :cats do |t|
+      t.string :name
+      t.integer :age
+      t.string :breed
+    end 
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151117223101) do
+ActiveRecord::Schema.define(version: 20200622223328) do
+
+  create_table "cats", force: :cascade do |t|
+    t.string  "name"
+    t.integer "age"
+    t.string  "breed"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
